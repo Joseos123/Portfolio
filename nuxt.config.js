cc17ac54ac2c3e0a368fd0c00ab165da6cf76040
Joshua Tsao <joshua@joseos.com>
2021-09-09 13:51:11 +0800

    Use vitejs for faster deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,9 @@ export default {
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [],
+  buildModules: [
+    'nuxt-vite'
+],
 
   // Modules: https://go.nuxtjs.dev/config-modules
   modules: []
