1bc6e9b64a9cba17e8f7bf66695e9a4fbfba9faf
Joshua Tsao <Joshua@Joseos.com>
2021-09-09 17:43:40 +0800

    startwork
    efgegrsgersergegegrr

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,17 +10,32 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
-    'nuxt-vite'
-],
+    'nuxt-vite',
+    // https://go.nuxtjs.dev/typescript
+    '@nuxt/typescript-build',
+    // https://go.nuxtjs.dev/tailwindcss
+    '@nuxtjs/tailwindcss',
+  ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: []
-};
+  modules: [
+  ],
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
