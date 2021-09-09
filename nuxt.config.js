03b32ae33d08a9fba29aeefe6bcac232b8882447
Joshua Tsao <joshuahj.tsao@gmail.com>
2021-09-09 23:38:48 +0800

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,7 @@ export default {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'theme-color', content: '#f72585' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
